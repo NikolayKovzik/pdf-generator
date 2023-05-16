5eb363cace5351b9b474ac2bc93deae5e1fcacde
Nikolay <nikolay.kovzik@gmail.com>
2023-05-16 11:29:33 +0300

    create basic structure & add swagger and validation pipe & install dependencies

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('pdf-generator API')
+    .setDescription('description for pdf-generator API application')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(PORT, () =>
+    console.log(`Server has been started on port: ${PORT}`),
+  );
 }
 bootstrap();
