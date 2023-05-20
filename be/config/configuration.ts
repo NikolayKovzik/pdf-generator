3b12f220702bc2886ffe3b9427d7ba7edbb3205f
Nikolay <nikolay.kovzik@gmail.com>
2023-05-20 13:27:44 +0300

    successful connect postgress & typeorm

--- be/config/configuration.ts
+++ be/config/configuration.ts
@@ -2,6 +2,13 @@ export const configuration = () => ({
   nodeEnv: process.env.NODE_ENV,
   hostName: process.env.HOST_NAME,
   port: parseInt(process.env.PORT, 10) || 4005,
+  database: {
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+  },
   // jwt: {
   //   secret: process.env.JWT_SECRET,
   //   expiresIn: process.env.JWT_EXPIRES_IN,
--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { configuration } from 'config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,21 @@ import { configuration } from 'config/configuration';
       envFilePath: `./config/env/${process.env.NODE_ENV}.env`,
       isGlobal: true,
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('database.host'),
+        port: +config.get('database.post'),
+        username: config.get('database.user'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        // entities: [],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
